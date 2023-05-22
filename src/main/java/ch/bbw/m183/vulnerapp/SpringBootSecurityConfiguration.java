97b251f834e375cb70f1c8eb01a5e56d844dfbe6
nnikolajj <greger.nikolaj@gmail.com>
2023-05-22 14:40:02 +0200

    Changes

---
+++ src/main/java/ch/bbw/m183/vulnerapp/SpringBootSecurityConfiguration.java
@@ -0,0 +1,16 @@
+package ch.bbw.m183.vulnerapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SpringBootSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        return http.build();
+    }
+}
--- src/main/java/ch/bbw/m183/vulnerapp/controller/UserController.java
+++ src/main/java/ch/bbw/m183/vulnerapp/controller/UserController.java
@@ -4,10 +4,13 @@ import java.util.Base64;
 
 import ch.bbw.m183.vulnerapp.datamodel.UserEntity;
 import ch.bbw.m183.vulnerapp.service.LoginService;
+import io.micrometer.common.util.internal.logging.InternalLogger;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.apache.catalina.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
 
+	private static final InternalLogger LOGGER = null;
 	private final LoginService loginService;
 
 	@GetMapping("/whoami")
-	public UserEntity whoami(@AuthenticationPrincipal User user) {
+	public UserEntity whoami(@AuthenticationPrincipal User user, HttpServletRequest request) {
+		CsrfToken token = (CsrfToken) request.getAttribute("_csrf");
+		LOGGER.info("{}={}", token.getHeaderName(), token.getToken());
 		return loginService.whoami(user.getUsername());
 	}
 }
